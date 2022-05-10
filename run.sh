ba57346fe9e116ca2e9e421694dd8295548882c0
tetneo003 <105028467+tetneo003@users.noreply.github.com>
2022-05-10 16:28:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 /usr/local/tomcat/bin/catalina.sh run &
-ngrok config add-authtoken 28QPxo5JqoV6wWKid3GSuD79X7j_4WttKmnxxvcGCy2BF9uEk
+ngrok config add-authtoken 28QPxo5JqoV6wWKid3GSuD79X7j_4WttKmnxxvcGCy2BF9uEk &
 ngrok tcp 8080 &
 
 
